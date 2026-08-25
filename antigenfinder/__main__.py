957f80553cc473a956347fda5b373358b8f30aa2
bbimber <bbimber@gmail.com>
2026-08-25 11:20:06 -0700

    Update arguments

--- antigenfinder/__main__.py
+++ antigenfinder/__main__.py
@@ -18,22 +18,22 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
 
     prepare_gtf_parser = subparsers.add_parser('prepare_gtf')
-    prepare_gtf_parser.add_argument('--gtf_file', help='The path to the GTF file.', type=str, default=[])
-    prepare_gtf_parser.add_argument('--fasta_file', help='The path to the genome FASTA file.', type=str, default=[])
-    prepare_gtf_parser.add_argument('--cache_dir', help='The path where the output will be created.', type=str, default=[])
-    prepare_gtf_parser.add_argument('--skip_processing_if_cached', help='An optional flag to skip re-processing. If the GFF DB exists in --cache_dir, it will be re-used, instead of re-processing the GTF, ', type=bool, default=False)
-    prepare_gtf_parser.add_argument('--debug_output', help='If provided, all inferred AA sequences will be written to this FASTA file. This can help troubleshoot processing of a GTF.', type=str, default=[])
+    prepare_gtf_parser.add_argument('--gtf_file', help='The path to the GTF file.', type=str, required=True)
+    prepare_gtf_parser.add_argument('--fasta_file', help='The path to the genome FASTA file.', type=str, required=True)
+    prepare_gtf_parser.add_argument('--cache_dir', help='The path where the output will be created.', type=str, required=False)
+    prepare_gtf_parser.add_argument('--skip_processing_if_cached', help='An optional flag to skip re-processing. If the GFF DB exists in --cache_dir, it will be re-used, instead of re-processing the GTF, ', action="store_true", default=False, required=False)
+    prepare_gtf_parser.add_argument('--debug_output', help='If provided, all inferred AA sequences will be written to this FASTA file. This can help troubleshoot processing of a GTF.', type=str, required=False)
 
     process_vcf_parser = subparsers.add_parser('process_vcf')
-    process_vcf_parser.add_argument('--vcf_file', help='The path to the VCF file.', type=str, default=[])
-    process_vcf_parser.add_argument('--source_sample', help='The name of the sample to test for neoantigens.', type=str, default=[])
-    process_vcf_parser.add_argument('--aa_flank_window', help='The number of amino acids to report on either side of the variant', type=int, default=10)
-    process_vcf_parser.add_argument('--output_file', help='The path where the output TSV will be written.', type=str, default=[])
+    process_vcf_parser.add_argument('--vcf_file', help='The path to the VCF file.', type=str, required=True)
+    process_vcf_parser.add_argument('--source_sample', help='The name of the sample to test for neoantigens.', type=str, required=True)
+    process_vcf_parser.add_argument('--aa_flank_window', help='The number of amino acids to report on either side of the variant', type=int, default=10, required=False)
+    process_vcf_parser.add_argument('--output_file', help='The path where the output TSV will be written.', type=str, required=True)
 
-    process_vcf_parser.add_argument('--gtf_file', help='The path to the GTF file.', type=str, default=[])
-    process_vcf_parser.add_argument('--fasta_file', help='The path to the genome FASTA file.', type=str, default=[])
-    process_vcf_parser.add_argument('--cache_dir', help='The path where the parsed GTF/FASTA output read or created.', type=str, default=[])
-    process_vcf_parser.add_argument('--skip_processing_if_cached', help='An optional flag to skip re-processing. If the GFF DB exists in --cache_dir, it will be re-used, instead of re-processing the GTF, ', type=bool, default=False)
+    process_vcf_parser.add_argument('--gtf_file', help='The path to the GTF file.', type=str, required=True)
+    process_vcf_parser.add_argument('--fasta_file', help='The path to the genome FASTA file.', type=str, required=True)
+    process_vcf_parser.add_argument('--cache_dir', help='The path where the parsed GTF/FASTA output read or created.', type=str, required=False)
+    process_vcf_parser.add_argument('--skip_processing_if_cached', help='An optional flag to skip re-processing. If the GFF DB exists in --cache_dir, it will be re-used, instead of re-processing the GTF, ', action="store_true", default=False, required=False)
 
     args = parser.parse_args()
 
@@ -54,4 +54,4 @@ if __name__ == "__main__":
 
     elif args.subcommand == 'process_vcf':
         tc = TranscriptCache(gff_file = args.gtf_file, fasta_file = args.fasta_file, cache_dir = args.cache_dir, skip_if_exists = args.skip_processing_if_cached)
-        results = process_vcf(vcf_file = args.vcf_file, source_sample = args.sample_name, transcript_cache = tc, aa_flank_window = args.aa_flank_window, output_file=args.output_file)
+        results = process_vcf(vcf_file = args.vcf_file, source_sample = args.source_sample, transcript_cache = tc, aa_flank_window = args.aa_flank_window, output_file=args.output_file)
